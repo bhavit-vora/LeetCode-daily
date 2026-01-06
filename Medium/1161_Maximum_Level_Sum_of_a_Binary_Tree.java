a0149ed1ed2aa0c482c40b4a400b9418e8d4b3ea
bhavit-vora <bhavitvora1990@gmail.com>
2026-01-06 18:56:44 +0530

    added a util class

--- Medium/1161_Maximum_Level_Sum_of_a_Binary_Tree.java
+++ Medium/1161_Maximum_Level_Sum_of_a_Binary_Tree.java
@@ -4,19 +4,7 @@
 */
 
 import java.util.*;
-
-public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
+import Utils.TreeNode;
 
 class Solution {
     public int maxLevelSum(TreeNode root) {
