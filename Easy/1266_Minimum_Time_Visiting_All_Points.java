6352fa7b53ec5cfdb8aeff03887ce6c0f08422ae
bhavit-vora <bhavitvora1990@gmail.com>
2026-01-12 04:33:15 +0000

    12-01-2026

---
+++ Easy/1266_Minimum_Time_Visiting_All_Points.java
@@ -0,0 +1,16 @@
+/*
+    TC: O(n)
+    SC: O(1)
+*/
+
+class Solution {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int time = 0;
+
+        for(int i = 0; i < points.length - 1; i ++) {
+            time += Math.max(Math.abs(points[i+1][0] - points[i][0]), Math.abs(points[i+1][1] - points[i][1])); 
+        }
+
+        return time;
+    }
+}
