6792f078ab97dc36b6547587922fa67afe29b83f
bhavit-vora <bhavitvora1990@gmail.com>
2026-02-05 04:39:24 +0000

    05-02-26

---
+++ Easy/3379_Transformed_Array.java
@@ -0,0 +1,17 @@
+/*
+    TC: O(n)
+    SC: O(n)
+*/
+
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        for(int i = 0; i < n; i ++) {
+            ans[i] = nums[((i + nums[i]) % n + n ) % n];    
+        }
+
+        return ans;
+    }
+}
