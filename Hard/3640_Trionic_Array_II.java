359120ce20ae162e2f36cb4addaa3a1f08706a54
bhavit-vora <bhavitvora1990@gmail.com>
2026-02-06 12:25:25 +0000

    alterate approach

--- Hard/3640_Trionic_Array_II.java
+++ Hard/3640_Trionic_Array_II.java
@@ -5,6 +5,8 @@
 
 import java.util.*;
 
+// Approach 1
+
 class Solution {
     int n;
     long[][] memo;
@@ -68,5 +70,57 @@ class Solution {
         }
 
         return solve(0, 0, nums);
+    }
+}
+
+// Approach 2
+
+class Solution2 {
+    public long helper(int[] nums, int i, boolean ok) {
+        if(i == -1 || i == nums.length) return -10_000_000_000_000L;
+        if(ok) {
+            if(nums[i] >= nums[i+1]) return -10_000_000_000_000L;
+            if(nums[i] < 0 && nums[i] < nums[i+1]) return nums[i];
+            long sum = 0l;
+            while(i >= 0 && nums[i] >= 0 && nums[i] < nums[i+1]) sum += nums[i--];
+            return sum;
+        } else {
+            if(nums[i] <= nums[i-1]) return -10_000_000_000_000L;
+            long sum = nums[i++];
+            long temp = sum;
+            while(i < nums.length && nums[i] > nums[i-1]) {
+                temp += nums[i];
+                sum = Math.max(sum, temp);
+                i++;
+            }
+            return sum;
+        }
+    }
+
+    public long maxSumTrionic(int[] nums) {
+        int n = nums.length;
+        long max = Long.MIN_VALUE / 2000;
+
+        int l = 0;
+        while(l < n) {
+            int r = l + 1;
+            long sum = nums[l];
+            while(r < n && nums[r] < nums[r-1]) sum += nums[r++];
+
+            if(r != l + 1) {
+                long left = helper(nums, l-1, true);
+                long right = helper(nums, r, false);
+                if(left != -10_000_000_000_000L && 
+                   right != -10_000_000_000_000L) 
+                     if(sum + left + right > max) {
+                        max = Math.max(max, sum + left + right);
+                        System.out.println(l);
+                     }
+            }
+
+            l = r;
+        }
+
+        return max;
     }
 }
