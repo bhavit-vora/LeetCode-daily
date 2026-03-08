6720d316b08e4d6bb5d8205eb9e297f618e7f685
bhavit-vora <bhavitvora1990@gmail.com>
2026-03-08 04:38:35 +0000

    08-03-2026

---
+++ Medium/1980_Find_Unique_Binary_String.java
@@ -0,0 +1,15 @@
+/*
+    TC: O(n)
+    SC: O(n)
+*/
+
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < nums.length; ++ i) {
+            sb.append(nums[i].charAt(i) == '0'? '1' : '0');
+        }
+
+        return sb.toString();
+    }
+}
