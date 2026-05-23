bb3f4f536d218b20a672b244edba6cc3b5a74ccf
bhavit-vora <bhavitvora1990@gmail.com>
2026-05-23 07:16:36 +0000

    23-05-2026

---
+++ Easy/1752_Check_if_Array_Is_Sorted_and_Rotated.java
@@ -0,0 +1,17 @@
+/*
+    TC: O(n)
+    SC: O(1)
+*/
+
+class Solution {
+    public boolean check(int[] nums) {
+        int n = nums.length;
+
+        int c = 0;
+        for(int i = 0; i < n; ++ i) {
+            if(nums[i] > nums[(i+1) % n]) c++;
+        }
+
+        return c < 2;
+    }
+}
