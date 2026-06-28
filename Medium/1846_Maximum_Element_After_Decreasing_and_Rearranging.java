9fc92ca63b2d4ad66b0340989903280471ca14b3
bhavit-vora <bhavitvora1990@gmail.com>
2026-06-28 18:33:45 +0000

    28-06-2026

---
+++ Medium/1846_Maximum_Element_After_Decreasing_and_Rearranging.java
@@ -0,0 +1,17 @@
+/*
+    TC: O(n * logn)
+    SC: O(1)
+*/
+
+import java.util.*;
+
+class Solution {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        Arrays.sort(arr);
+        arr[0] = 1;
+        for(int i = 1; i < arr.length; i ++) {
+            arr[i] = Math.min(arr[i-1] + 1, arr[i]);
+        }
+        return arr[arr.length-1];
+    }
+}
